d7bc4412420c2777948251c3a25ba44a7d8dac74
Vikash Singh <vikash43366@gmail.com>
2024-08-25 11:57:26 +0530

    Add files via upload

---
+++ AreaOfSquare.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int a,area;
+    cout<<"Enter The Value Of Side : ";
+    cin>>a;
+    area=a*a;
+    cout<<"Area of Square : "<<area;
+    return 0;
+}
---
+++ PerimeterOfSquare.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+// a is side of square
+int main(){
+    int a,perimeter;
+    cout<<"Enter Side : ";
+    cin>>a;
+    perimeter=(4*a);
+    cout<<"Perimeter of Square : "<<perimeter;
+    return 0;
+}
